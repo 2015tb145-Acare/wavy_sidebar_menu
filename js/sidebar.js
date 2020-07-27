cfb9041e8ea32b8341326ca0b92238ed5c330f04
Acare <alban.care@gmail.com>
2020-07-27 07:20:38 +0200

    initial commit

---
+++ js/sidebar.js
@@ -0,0 +1,49 @@
+(function () {
+    
+    let path = document.querySelector('#sidebar-wave path')
+    let sidebarLinks = document.querySelectorAll('.sidebar a')
+    let from = path.getAttribute('d')
+    let to = path.dataset['to']
+    let options = {
+        type: dynamics.easeOut,
+        duration: 454,
+        friction: 253
+    }
+
+    let button = document.querySelector('#menu')
+    let sidebarOpened = false
+
+    button.addEventListener('click', function (e) {
+        e.stopPropagation()
+        e.preventDefault()
+        document.body.classList.add('has-sidebar')
+
+        dynamics.animate(path, {
+            d: to
+        }, options)
+
+        sidebarLinks.forEach(function (link, i) {
+            dynamics.animate(link, {
+                translateX : 0
+            }, Object.assign({}, options, {delay: 50 * i, duration: 300}))
+        })
+
+        sidebarOpened = true
+    })
+
+    document.body.addEventListener('click', function () {
+        if (sidebarOpened) {
+            document.body.classList.remove('has-sidebar')
+            dynamics.animate(path, {
+                d: from
+            }, options)
+
+            sidebarLinks.forEach(function (link, i) {
+                dynamics.animate(link, {
+                    translateX : 200
+                }, options)
+            })
+        }
+    })
+
+})()
